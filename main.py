107f97e58c8eba1801ad1171c5e3a9e4d66e99de
548821 <3270358320@qq.com>
2025-09-19 10:44:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-# 在这个文件里编写代码
+def main():
+  earth_weight=0
+  for i in range(10):
+    earth_weight +=0.5
+    moon_weight=earth_weight*0.165
+    print("{}年地球上体重：{}kg".format(i+1,earth_weight))
+    print("{}年月球上体重：{}kg".format(i+1,moon_weight))
+    
